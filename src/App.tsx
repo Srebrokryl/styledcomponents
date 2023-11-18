572dd05416e0697025186a4ccac55c56214e2ab3
Srebrokryl <nikitakol2000@mail.ru>
2023-11-18 11:18:17 +0400

    Create SuperBtn

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <div className="App">
             <Box>
                 <StyledBtn>Hello</StyledBtn>
+                <SuperBtn></SuperBtn>
             </Box>
         </div>
     );
@@ -19,13 +20,16 @@ const StyledBtn = styled.button`
   border: none;
   background-color: fuchsia;
   padding: 10px 20px;
-  border-radius: 5px;
   color: snow;
   font-size: 2rem;
   font-weight: bold;
+  `
+const SuperBtn = styled(StyledBtn) `
+  border-radius: 5px;
+
 `
 
-const Box = styled.div`
+const Box = styled.div `
   display: flex;
   height: 100vh;
   justify-content: center;
