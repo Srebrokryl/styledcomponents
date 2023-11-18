f4bf43b9c7e9b4874f2616ac727bdeec6a3bdf05
Srebrokryl <nikitakol2000@mail.ru>
2023-11-18 20:19:21 +0400

    Add primary and outlined

--- src/components/Button.styled.tsx
+++ src/components/Button.styled.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import {MyAnimation} from "../styles/animations/Animations";
 
 type StyledBtnPropsType = {
@@ -11,32 +11,47 @@ type StyledBtnPropsType = {
 export const StyledBtn = styled.button<StyledBtnPropsType>`
   //border: none;
   //background-color: fuchsia;
-/ / background-color: ${props => props.color || "fuchsia"};
+    //background-color: ${props => props.color || "fuchsia"};
+  border-radius: 10px;
   padding: 10px 20px;
   //color: snow;
   //font-size: 2rem;
   font-size: ${props => props.fontSize || "2rem"};
   font-weight: bold;
 
-  &:hover {
-    background-color: rgb(52, 65, 112);
-  }
+  
 
   /* &:last-child {
      background-color: #5a689d;
    }*/
 
+  ${props => props.outlined && css<StyledBtnPropsType>`
+    border: 2px solid ${props => props.color || "fuchsia"};
+    color: ${props => props.color || "fuchsia"};
+    background-color: transparent;
+    
+    &:hover {
+      border-color: green;
+      color: midnightblue;
+      background-color: transparent;
+    }
+  `}
   
-  
-  
-  //primary
-  background-color: ${props => props.color || "fuchsia"};
-  color: snow;
+  ${props => props.primary && css<StyledBtnPropsType>`
+    background-color: ${props => props.color || "fuchsia"};
+    color: snow;
 
-  //outlined
-  border: 2px solid ${props => props.color || "fuchsia"};
-  color: ${props => props.color || "fuchsia"};
-  background-color: transparent;
+    &:hover {
+      background-color: rgb(52, 65, 112);
+    }
+  `}
+
+
+  //primary
+
+
+
+
 `
 
 export const SuperBtn = styled(StyledBtn)`
